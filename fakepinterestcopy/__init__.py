81c840efddcd7320b337d5f3dca0169a7ef560b1
Felipeils-debug <idercifelipe2@gmail.com>
2024-08-30 21:45:22 -0300

    alteração banco de dados

--- fakepinterestcopy/__init__.py
+++ fakepinterestcopy/__init__.py
@@ -2,10 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = "401a2079791c1d39fcfa41f5e944b3aa"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 database = SQLAlchemy(app)
